4b184ba0349312a666b31590389c6946850466dc
ArnoldSilvaC <66580458+ElProgramadorQueNoEsPro@users.noreply.github.com>
2021-12-03 18:41:05 -0500

    Update slider.js

--- public/funciones/slider.js
+++ public/funciones/slider.js
@@ -1,26 +1,126 @@
-var slideIndex = 1;
-showSlides(slideIndex);
+/*global console*/
 
-function plusSlides(n) {
-  showSlides(slideIndex += n);
+var gallary = document.getElementById('slider-screen'),
+	next    = document.getElementById('next-slide'),
+	prev    = document.getElementById('prev-slide'),
+	slide   = document.querySelectorAll('.slider-img'),
+	dataImg = document.querySelectorAll('.slider-img'),
+	balls  = document.querySelectorAll('.bullets li'),
+	slideTimer,
+	currentSlide = 1,
+	currentBall = 1;
+
+slide[0].classList.add('active');
+balls[0].classList.add('active');
+
+next.onclick = toNext;
+prev.onclick = toPrev;
+
+	function palySliderAtuomatic () {
+		'use strict';
+		removeActive();
+		if(currentSlide > slide.length){
+			currentSlide = 1;
+		}		
+		slide.forEach(function(i,k,slide){
+			if(slide[k].getAttribute('data-img') == currentSlide){
+				slide[k].classList.add('active');
+				balls[k].classList.add('active');
+				if(currentSlide == 1){
+					gallary.setAttribute('style',`transform: translateX(0px)`);
+				} else {
+					gallary.setAttribute('style',`transform: translateX(calc(-1200px * ${currentSlide - 1}))`);
+				}			
+			}
+		});		
+	}
+
+slideTimer = setInterval(function(){
+				currentSlide++;
+				palySliderAtuomatic();
+			},3000);
+
+function toNext (){
+	'use strict';
+	clearInterval(slideTimer);
+	if(currentSlide < slide.length){
+		currentSlide += 1;
+		currentBall  += 1;
+		check();
+	} else {
+		currentSlide = 1;
+		currentBall  = 1;
+		check();
+	}
+	slideTimer = setInterval(function(){
+				currentSlide++;
+				palySliderAtuomatic();
+			},3000);
 }
 
-function currentSlide(n) {
-  showSlides(slideIndex = n);
+// To get the previous slide
+function toPrev (){
+	'use strict';
+	clearInterval(slideTimer);
+	if(currentSlide == 1){
+		currentSlide = slide.length;
+		currentBall  = balls.length;
+		check();
+	} else {
+		currentSlide -= 1;
+		currentBall  -= 1;
+		check();
+	}
+	slideTimer = setInterval(function(){
+				currentSlide++;
+				palySliderAtuomatic();
+			},3000);
 }
 
-function showSlides(n) {
-  var i;
-  var slides = document.getElementsByClassName("mySlides");
-  var dots = document.getElementsByClassName("dot");
-  if (n > slides.length) {slideIndex = 1}    
-  if (n < 1) {slideIndex = slides.length}
-  for (i = 0; i < slides.length; i++) {
-      slides[i].style.display = "none";  
-  }
-  for (i = 0; i < dots.length; i++) {
-      dots[i].className = dots[i].className.replace(" active", "");
-  }
-  slides[slideIndex-1].style.display = "block";  
-  dots[slideIndex-1].className += " active";
+// click in any down ball bar
+
+balls.forEach(function (item, key, balls){
+		balls[key].onclick = function (){
+			'use strict';
+			clearInterval(slideTimer);
+			removeActive();
+			currentSlide = this.getAttribute('data-bull');
+			currentBall  = this.getAttribute('data-bull');
+			check();
+			slideTimer = setInterval(function(){
+				currentSlide++;
+				palySliderAtuomatic();
+			},3000);
+		}
+		
+	});
+
+
+//the main function to move the slider
+function check (){
+	'use strict';
+	removeActive();
+	slide.forEach(function(i,k,slide){
+		if(slide[k].getAttribute('data-img') == currentSlide){
+			slide[k].classList.add('active');
+			balls[k].classList.add('active');
+			if(currentSlide == 1){
+				gallary.setAttribute('style',`transform: translateX(0px)`);
+			} else {
+				gallary.setAttribute('style',`transform: translateX(calc(-1200px * ${currentSlide - 1}))`);
+			}			
+		}
+	});
+}
+
+
+function removeActive () {
+	'use strict';
+	for (var i = 0; i < slide.length; i++) {
+		slide[i].classList.remove('active');
+	}
+	
+	for(var b = 0; b < balls.length; b++) {
+		balls[b].classList.remove('active');
+	}
 }
